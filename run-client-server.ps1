bc0f3be9351754b3640683465f588c5611b0c540
Nehliin <oskar.d.nehlin@gmail.com>
2021-04-17 15:31:12 +0200

    Update run script

--- run-client-server.ps1
+++ run-client-server.ps1
@@ -1,5 +1,6 @@
 ﻿Write-host "Starting server"
 Start-Process cargo -ArgumentList "run", "--bin", "server"
-Start-Sleep -Seconds 3
+Start-Sleep -Seconds 1
 Write-host "Starting client"
+$env:RUST_BACKTRACE = 1
 cargo run --bin client
